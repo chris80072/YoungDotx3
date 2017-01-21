29001dab2e8303ac64d44967879c2646ffdfed5e
Chris <chris80072@gmail.com>
2017-01-21 17:51:25 +0800

    adjest menu

--- YoungDotx3/Controllers/HomeController.cs
+++ YoungDotx3/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace YoungDotx3.Controllers
             return View();
         }
 
+        public ActionResult Intro()
+        {
+            return View();
+        }
+
         //public ActionResult About()
         //{
         //    ViewBag.Message = "Your application description page.";
